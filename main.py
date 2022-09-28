c22f2202ade5ac600c34629691eed9fbbdf5cca6
gabriel-kimutai <gabrielkimutai997@gmail.com>
2022-09-28 12:02:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame as pg
+import moderngl as mgl
+import sys
+from model import *
+from camera import Camera
+
+
+class GraphicsEngine:
+    def __init__(self, win_size=(1280, 800)):
+        # init pygame modules
+        pg.init()
+        # window size
+        self.WIN_SIZE = win_size
+        # set opengl attr
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(
+            pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE
+        )
+        # create opengl context
+        pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
+        # detect and use existing opengl context
+        self.ctx = mgl.create_context()
+        # create object to track time
+        self.clock = pg.time.Clock()
+        self.time = 0
+
+        # camera
+        self.camera = Camera(self)
+        # scene
+        self.scene = Cube(self)
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (
+                event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE
+            ):
+                self.scene.destroy()
+                pg.quit()
+                sys.exit()
+
+    def render(self):
+        # clear framebuffer
+        self.ctx.clear(color=(0.08, 0.16, 0.18))
+        # render scene
+        self.scene.render()
+        # swap buffers
+        pg.display.flip()
+
+    def get_time(self):
+        self.time = pg.time.get_ticks() * 0.001
+
+    def run(self):
+        while True:
+            self.get_time()
+            self.check_events()
+            self.render()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    app = GraphicsEngine()
+    app.run()
---
+++ model.py
@@ -0,0 +1,181 @@
+import numpy as np
+import glm
+
+
+def get_vertex_data():
+    vertex_data = [(-0.6, -0.8, 0.0), (0.6, -0.8, 0.0), (0.0, 0.8, 0.0)]
+    vertex_data = np.array(vertex_data, dtype="f4")
+    return vertex_data
+
+
+class Triangle:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+        self.vbo = self.get_vbo()
+        self.shader_program = self.get_shader_program("default")
+        self.vao = self.get_vao()
+
+    def render(self):
+        self.vao.render()
+
+    def destroy(self):
+        self.vbo.release()
+        self.shader_program.release()
+        self.vao.release()
+
+    def get_vao(self):
+        vao = self.ctx.vertex_array(
+            self.shader_program, [(self.vbo, "3f", "in_position")]
+        )
+        return vao
+
+    def get_vbo(self):
+        vertex_data = get_vertex_data()
+        vbo = self.ctx.buffer(vertex_data)
+        return vbo
+
+    def get_shader_program(self, shader_name):
+        with open(f"shaders/{shader_name}.vert") as file:
+            vertex_shader = file.read()
+        with open(f"shaders/{shader_name}.frag") as file:
+            fragment_shader = file.read()
+
+        program = self.ctx.program(
+            vertex_shader=vertex_shader, fragment_shader=fragment_shader
+        )
+        return program
+
+
+def get_model_matrix():
+    m_model = glm.mat4()
+    return m_model
+
+
+class Cube:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+        self.vbo = self.get_vbo()
+        self.shader_program = self.get_shader_program("default")
+        self.vao = self.get_vao()
+        self.m_model = get_model_matrix()
+        self.on_init()
+
+    def update(self):
+        m_model = glm.rotate(self.m_model, self.app.time, glm.vec3(0, 1, 0))
+        self.shader_program['m_model'].write(m_model)
+
+    def on_init(self):
+        self.shader_program["m_proj"].write(self.app.camera.m_proj)
+        self.shader_program['m_view'].write(self.app.camera.m_view)
+        self.shader_program['m_model'].write(self.m_model)
+
+    def render(self):
+        self.update()
+        self.vao.render()
+
+    def destroy(self):
+        self.vbo.release()
+        self.shader_program.release()
+        self.vao.release()
+
+    def get_vao(self):
+        vao = self.ctx.vertex_array(
+            self.shader_program, [(self.vbo, "2f 3f", "in_texcoord_0", "in_position")]
+        )
+        return vao
+
+    def get_vertex_data(self):
+        vertices = [
+            (-1, -1, 1),
+            (1, -1, 1),
+            (1, 1, 1),
+            (-1, 1, 1),
+            (-1, 1, -1),
+            (-1, -1, -1),
+            (1, -1, -1),
+            (1, 1, -1),
+        ]
+        indices = [
+            (0, 2, 3),
+            (0, 1, 2),
+            (1, 7, 2),
+            (1, 6, 7),
+            (6, 5, 4),
+            (4, 7, 6),
+            (3, 4, 5),
+            (3, 5, 0),
+            (3, 7, 4),
+            (3, 2, 7),
+            (0, 6, 1),
+            (0, 5, 6),
+        ]
+        vertex_data = self.get_data(vertices, indices)
+
+        tex_coord = [(0, 0), (1, 1), (0, 1), (1, 0)]
+        tex_coord_indices = [(0, 2, 3), (0, 1, 2), (0, 2, 3), (0, 1, 2), (0, 1, 2), (2, 3, 0), (2, 3, 0), (2, 0, 1),
+                             (0, 2, 3), (0, 1, 2), (3, 1, 2), (3, 0, 1)]
+        tex_coord_data = self.get_data(tex_coord, tex_coord_indices)
+        vertex_data = np.hstack([tex_coord_data, vertex_data])
+
+        return vertex_data
+
+    @staticmethod
+    def get_data(vertices, indices):
+        data = [vertices[ind] for triangle in indices for ind in triangle]
+        return np.array(data, dtype="f4")
+
+    def get_vbo(self):
+        vertex_data = self.get_vertex_data()
+        vbo = self.ctx.buffer(vertex_data)
+        return vbo
+
+    def get_shader_program(self, shader_name):
+        with open(f"shaders/{shader_name}.vert") as file:
+            vertex_shader = file.read()
+        with open(f"shaders/{shader_name}.frag") as file:
+            fragment_shader = file.read()
+
+        program = self.ctx.program(
+            vertex_shader=vertex_shader, fragment_shader=fragment_shader
+        )
+        return program
+
+class Square:
+    def __init__(self, app):
+        self.app = app
+        self.ctx = app.ctx
+        self.vbo = self.get_vbo()
+        self.shader_program = self.get_shader_program("default")
+        self.vao = self.get_vao()
+
+    def render(self):
+        self.vao.render()
+
+    def destroy(self):
+        self.vbo.release()
+        self.shader_program.release()
+        self.vao.release()
+
+    def get_vao(self):
+        vao = self.ctx.vertex_array(
+            self.shader_program, [(self.vbo, "3f", "in_position")]
+        )
+        return vao
+
+    def get_vbo(self):
+        vertex_data = get_vertex_data()
+        vbo = self.ctx.buffer(vertex_data)
+        return vbo
+
+    def get_shader_program(self, shader_name):
+        with open(f"shaders/{shader_name}.vert") as file:
+            vertex_shader = file.read()
+        with open(f"shaders/{shader_name}.frag") as file:
+            fragment_shader = file.read()
+
+        program = self.ctx.program(
+            vertex_shader=vertex_shader, fragment_shader=fragment_shader
+        )
+        return program
